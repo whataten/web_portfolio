1fcac660d8a309636192fba943fe9e0654dc5e4f
whataten <tiktaalik135462@gmail.com>
2024-09-18 21:21:39 +0900

    service gateway server

---
+++ backend/servers/service-gateway-server/src/main/java/com/poscodx/msa/server/ServiceGatewayServer.java
@@ -0,0 +1,28 @@
+package com.poscodx.msa.server;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceGatewayServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGatewayServer.class, args);
+	}
+	
+    @Bean
+    TomcatServletWebServerFactory servletContainer() {
+		return new TomcatServletWebServerFactory() {
+			@Override
+			protected void customizeConnector(Connector connector) {
+				super.customizeConnector(connector);
+				connector.setParseBodyMethods("POST,PUT,DELETE");
+			}
+		};
+	}
+}
